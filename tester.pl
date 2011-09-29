048ad973a399047e30af38dc17337d61d03201f6
Michael Reed <mhr2126@gmail.com>
2011-09-29 05:25:12 -0400

    removed old files

--- tester.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-system('make', 'all');
-$threshold=111;
-
-system("./p1 two_objects.pgm $threshold test_binary.pgm");
-system("./p2 test_binary.pgm test_labeled.pgm");
-system("./p3 test_labeled.pgm test_db test_lines.pgm");
-system("./p4 test_labeled.pgm test_db test_recog1.pgm");
